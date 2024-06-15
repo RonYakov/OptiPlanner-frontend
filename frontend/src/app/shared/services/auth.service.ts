231a657e8b638bf5a087fcb44b73d83f6a8c516e
IdoHirschmann <ido.hirschmann@gmail.com>
2024-06-15 14:37:49 +0300

    ec2 deploy

--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import {map, BehaviorSubject} from "rxjs";
 import { SidebarService } from './sidebar.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +11,25 @@ export class AuthService {
   private loggedIn = new BehaviorSubject<boolean>(false);
   loggedIn$ = this.loggedIn.asObservable();
 
+
   constructor(private http: HttpClient,
               private sidebarService: SidebarService) { }
 
 
   signUp(name: string, email:string, password:string){
-    return this.http.post( `http://localhost:3000/auth/sign-up`,{name, email, password}).pipe(map((res: any) => res))
+    return this.http.post( environment.API_URL + `/auth/sign-up`,{name, email, password}).pipe(map((res: any) => res))
   }
 
   signUpWithGoogle(googleToken: string){
-    return this.http.post(`http://localhost:3000/auth/google-sign-up`, { token: googleToken }).pipe(map((res: any) => res));
+    return this.http.post(environment.API_URL + `/auth/google-sign-up`, { token: googleToken }).pipe(map((res: any) => res));
   }
 
   confirmRegister(email :String, confirmationCode: string){
-    return this.http.post( `http://localhost:3000/auth/confirm-register`,{email, confirmationCode}).pipe(map((res: any) => res))
+    return this.http.post( environment.API_URL + `/auth/confirm-register`,{email, confirmationCode}).pipe(map((res: any) => res))
   }
 
   login(email: string, password: string){
-    return this.http.post( `http://localhost:3000/auth/login`,{email, password}).pipe(
+    return this.http.post( environment.API_URL + `/auth/login`,{email, password}).pipe(
       map((res: any) => {
         // After a successful login, store the user ID in localStorage
         if (typeof localStorage !== 'undefined') {
@@ -39,7 +41,7 @@ export class AuthService {
   }
 
   signout(){
-    return this.http.post( `http://localhost:3000/auth/signout`,{}).pipe(
+    return this.http.post( environment.API_URL + `/auth/signout`,{}).pipe(
       map((res: any) => {
         // After a successful logout, clear the user data
         this.sidebarService.clearUserData();
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_URL: 'http://3.80.251.4:3000' // replace with your production API URL
+};
